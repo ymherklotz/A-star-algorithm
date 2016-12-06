1c36505258649bf89ca7c6e55bb5592e34b852f4
zedarider <ymherklotz@gmail.com>
2016-12-06 15:59:34 +0000

    adding files

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
  */
 
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
 
 using namespace std;
@@ -14,5 +16,38 @@ using namespace std;
 int main(int argc, char *argv[]) {
     cout << "executing " << argv[0] << endl;
     cout << "arguments given: " << argc-1 << endl;
+
+    sf::RectangleShape rect(sf::Vector2f(25, 25));
+    rect.setFillColor(sf::Color(100, 100, 100));
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "A* Algorithm");
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && rect.getPosition().x > 0) {
+            rect.move(-0.5f, 0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            rect.move(0.5f, 0);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            rect.move(0, -0.5f);
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            rect.move(0, 0.5f);
+        }
+
+
+        window.clear(sf::Color(47, 47, 47));
+        window.draw(rect);
+        window.display();
+    }
+    
     return 0;
 }
