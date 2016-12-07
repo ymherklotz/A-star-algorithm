dce13d7a63ecf8e2c0ceb4df5a13eee4f0849350
zedarider <ymherklotz@gmail.com>
2016-12-07 17:44:30 +0000

    adding comments

--- include/TileMap.hpp
+++ include/TileMap.hpp
@@ -1,3 +1,6 @@
+// Tile map class that creates vertices to form a grid full of
+// squares and adds texture to it.
+
 #ifndef TILEMAP_HPP
 #define TILEMAP_HPP
 
@@ -10,11 +13,19 @@ public:
 	TileMap();
 	~TileMap();
 
+	// loads the text file and then creates the vertex array and the assigns
+	// the texture to the vertex array. It also therefore defines the size
+	// of the grid.
 	bool load(const std::string& txt_file, const sf::Vector2f& txt_size, const sf::Vector2f& tile_size, const int *tiles, unsigned int width, unsigned int height);
 private:
+	// private function draw that overwrites the draw function in the
+	// Drawable function. This draws all the vertices to the window.
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
+	// vertex array that contains all the vertices.
 	sf::VertexArray m_vertices;
+	// texture that will be loaded onto the vertices. This will be associated
+	// to the vertices.
 	sf::Texture m_texture;
 };
 
--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -7,27 +7,31 @@ TileMap::~TileMap() {
 }
 
 bool TileMap::load(const std::string& txt_file, const sf::Vector2f& txt_size, const sf::Vector2f& tile_size, const int *tiles, unsigned int width, unsigned int height) {
+	// load texture from the file
 	if(!m_texture.loadFromFile(txt_file))
 		return false;
 
+	// set the sizes and type of the vertex array.
 	m_vertices.setPrimitiveType(sf::Quads);
+	// multiply it by 4 because a square has 4 vertices.
 	m_vertices.resize(width * height * 4);
 
+	// go through the array and make vertices.
 	for(unsigned int i = 0; i < width; ++i)
 		for(unsigned int j = 0; j < height; ++j) {
 			// get the colour of the tile
 			int tile_number = tiles[i + j * width];
 
-			// get pointer to vertex of current quad
+			// get pointer to vertex of current quad.
 			sf::Vertex *quad = &m_vertices[(i + j * width) * 4];
 
-			// define the quads
+			// define the quads.
 			quad[0].position = sf::Vector2f(i * tile_size.x, j * tile_size.y);
 			quad[1].position = sf::Vector2f((i + 1) * tile_size.x, j * tile_size.y);
 			quad[2].position = sf::Vector2f((i + 1) * tile_size.x, (j + 1) * tile_size.y);
 			quad[3].position = sf::Vector2f(i * tile_size.x, (j + 1) * tile_size.y);
 
-			// define texture
+			// define texture coordinates.
 			quad[0].texCoords = sf::Vector2f(tile_number * txt_size.x, 0);
 			quad[1].texCoords = sf::Vector2f((tile_number + 1) * txt_size.x, 0);
 			quad[2].texCoords = sf::Vector2f((tile_number + 1) * txt_size.x, txt_size.y);
@@ -37,8 +41,11 @@ bool TileMap::load(const std::string& txt_file, const sf::Vector2f& txt_size, co
 }
 
 void TileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+	// get the transform and set it to the states.
 	states.transform = getTransform();
+	// assign it the texture as well.
 	states.texture = &m_texture;
 
+	// draw the vertices with the transforms and textures.
 	target.draw(m_vertices, states);
 }
